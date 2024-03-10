c59330f2492ff37d983e926ec3c424b32c27ce73
riyanshgupta <riyanshgupta794@gmail.com>
2024-03-10 14:15:42 +0530

    commit

--- Metal/settings.py
+++ Metal/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -129,7 +129,7 @@ import os
 STATICFILES_DIRS = [
     BASE_DIR/"static/"
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 INSTALLED_APPS += [
     'home.apps.HomeConfig'
 ]
